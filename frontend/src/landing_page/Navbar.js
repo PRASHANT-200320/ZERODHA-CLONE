6e09e944ca2325298f9049fdc6a789c550a5661f
PRASHANT-200320 <prashant.shringi20@gmail.com>
2025-07-16 23:23:44 +0530

    update Navbar dashboard href

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -46,7 +46,7 @@ function Navbar({ isLoggedIn, handleLogout }) {
                 <li className="nav-item">
                  <a
                     className="nav-link"
-                    href={process.env.REACT_APP_DASHBOARD_URL}
+                    href="https://zerodha-clone-dashboard-h8l5v9e49-prashant-200320s-projects.vercel.app"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
